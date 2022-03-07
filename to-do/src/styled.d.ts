a7bea64aa62534ac22de1d7d3861a8d40b0a18ed
dblee <danbi.db@gmail.com>
2022-03-07 13:17:23 +0900

    add todo

---
+++ to-do/src/styled.d.ts
@@ -0,0 +1,11 @@
+import "styled-components";
+
+// extend
+declare module "styled-components" {
+  export interface DefaultTheme {
+    bgColor: string;
+    textColor: string;
+    accentColor: string;
+    cardBgColor: string;
+  }
+}
